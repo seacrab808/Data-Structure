93a4f31b927baa01a04e63f63e0156f5198a402b
seacrab808 <seacrab808@naver.com>
2024-04-15 19:53:08 +0900

    4.2 구조체 배열 스택 프로그램

--- Structure/Stack_4-2.c
+++ Structure/Stack_4-2.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 
 #define MAX_STACK_SIZE 100
+#define MAX_STRING 100
 
-typedef int element;
+typedef struct {
+	int student_no;
+	char name[MAX_STRING];
+	char address[MAX_STRING];
+} element;
+
 element stack[MAX_STACK_SIZE];
 int top = -1;
 
@@ -51,11 +57,18 @@ element peek()
 
 int main(void)
 {
-	push(1);
-	push(2);
-	push(3);
-	printf("%d\n", pop());
-	printf("%d\n", pop());
-	printf("%d\n", pop());
+	element ie = {
+		20190001,
+		"So",
+		"Seoul"
+	};
+
+	element oe;
+
+	push(ie);
+	oe = pop();
+	printf("학번: %d\n", oe.student_no);
+	printf("이름: %s\n", oe.name);
+	printf("주소: %s\n", oe.address);
 	return 0;
 }
